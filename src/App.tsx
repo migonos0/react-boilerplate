a6f5b27d6717166681b3bc5560fed90227a785e6
Miguel Rodríguez <miguelrodrii@outlook.com>
2023-05-12 10:05:57 -0500

    Added base page for functioning post creation.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,94 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useForm } from "react-hook-form";
+import { Button } from "./app/components/button";
+import { Input } from "./app/components/input";
+import { Subtitle } from "./app/components/subtitle";
+import { Title } from "./app/components/title";
+import { MainLayout } from "./app/layouts/main.layout";
+import { usePosts } from "./state/post.state";
+import {
+  CreatePostInput,
+  CreatePostSchema,
+} from "./schemas/inputs/create-post.schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ErrorMessage } from "./app/components/error-message";
+import { useInputRequest } from "./hooks/use-input-request";
+import { createPost } from "./services/post.service";
+import { useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const { posts, addPost } = usePosts();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<CreatePostInput>({
+    resolver: zodResolver(CreatePostSchema),
+  });
+  const { response, setInput } = useInputRequest(createPost);
+
+  useEffect(() => {
+    if (!response) return;
+    addPost(response);
+  }, [response, addPost]);
+
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <MainLayout>
+      <div className="flex flex-col gap-y-8 p-4">
+        <form
+          onSubmit={handleSubmit(setInput)}
+          className="flex flex-col gap-y-2"
+        >
+          <Title value="Create" />
+          <div className="flex gap-x-1">
+            <div>
+              <Input
+                {...register("author")}
+                placeholder="Author"
+                className="flex-grow"
+              />
+              <ErrorMessage error={errors.author?.message} />
+            </div>
+            <div>
+              <Input
+                {...register("title")}
+                placeholder="Title"
+                className="flex-grow"
+              />
+              <ErrorMessage error={errors.title?.message} />
+            </div>
+          </div>
+          <Button type="submit">Create</Button>
+        </form>
+        <div className="flex flex-row gap-x-8">
+          <div className="flex-1">
+            <Title value="Read" />
+            <div>
+              {posts?.map((post, index) => (
+                <div
+                  className="flex flex-col border-slate-200 border-2 p-2 rounded-lg"
+                  key={index}
+                >
+                  <div>
+                    <Subtitle value={post.author} />
+                    <p key={index}>{post.title}</p>
+                  </div>
+                  <Button>Select</Button>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <div className="flex-1">
+            <div>
+              <Title value="Update" />
+            </div>
+            <div>
+              <Title value="Delete" />
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </MainLayout>
   );
 }
 
