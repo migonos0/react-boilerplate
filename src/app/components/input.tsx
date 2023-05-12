789dcc06b9c68ddf5cdc2b3c9c65e48bee907bc9
Miguel Rodríguez <miguelrodrii@outlook.com>
2023-05-11 21:46:16 -0500

    Modified input component in order to use forward ref react function.

--- src/app/components/input.tsx
+++ src/app/components/input.tsx
@@ -1,15 +1,21 @@
 import classNames from "classnames";
-import { InputHTMLAttributes } from "react";
+import { InputHTMLAttributes, LegacyRef, forwardRef } from "react";
 
-export const Input = (props: InputHTMLAttributes<HTMLInputElement>) => {
-  const { className, ...inputProps } = props;
-  return (
-    <input
-      className={classNames(
-        className,
-        "border-slate-200 border-2 rounded-xl px-2 focus:border-slate-400 hover:border-slate-300 outline-none"
-      )}
-      {...inputProps}
-    />
-  );
-};
+export const Input = forwardRef(
+  (
+    props: InputHTMLAttributes<HTMLInputElement>,
+    ref: LegacyRef<HTMLInputElement>
+  ) => {
+    const { className, ...inputProps } = props;
+    return (
+      <input
+        ref={ref}
+        className={classNames(
+          className,
+          "border-slate-200 border-2 rounded-xl px-2 focus:border-slate-400 hover:border-slate-300 outline-none"
+        )}
+        {...inputProps}
+      />
+    );
+  }
+);
